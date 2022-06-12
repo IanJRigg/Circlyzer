4c4b6b4de60a94b4d77c48a0e36cad2ed3b50c4a
IanJRigg <ianjrigg@gmail.com>
2022-06-12 17:07:28 -0500

    Add Network API

--- include/circuit_analyzer/network.h
+++ include/circuit_analyzer/network.h
@@ -13,10 +13,17 @@
 struct Element;
 struct Node;
 
+enum class Entity_Type
+{
+    Element,
+    Node
+};
+
 struct Unique_Entity
 {
     uint32_t uid;
     std::string alias;
+    Entity_Type type;
 };
 
 struct Element : Unique_Entity
@@ -33,17 +40,42 @@ struct Node : Unique_Entity
 class Network
 {
 public:
+	Network();
+	virtual ~Network() = default;
+
+	// Create Functions
+	uint32_t create_node();
+	uint32_t create_element();
+
+	// Read Functions
 	const Node& get_node(uint32_t uid) const;
 	const Element& get_element(uint32_t uid) const;
 
+	const Node& get_node(const std::string& alias) const;
+	const Element& get_element(const std::string& alias) const;
+
+	// Update Functions
 	void create_connection_between(uint32_t first_entity, uint32_t second_entity);
 	void delete_connection_between(uint32_t first_entity, uint32_t second_entity);
 
-	void find_loops() const;
+	void create_connection_between(const std::string& first_entity, 
+		                           const std::string& second_entity);
+
+	void delete_connection_between(const std::string& first_entity,
+		                           const std::string& second_entity);
+
+	// Destroy Functions
+    void destroy_node(uint32_t uid);
+    void destroy_element(uint32_t uid);
+
+    void destroy_node(const std::string& alias);
+    void destroy_element(const std::string alias);
 
 private:
 	std::map<uint32_t, std::shared_ptr<Unique_Entity>> entity_table;
 	std::map<std::string, uint32_t> alias_to_id_table;
+
+	uint32_t id_incrementer;
 };
 
 #endif
