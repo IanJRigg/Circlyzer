613cc5e8b5e6e5626c012e60e77aa0bf541157bb
IanJRigg <ianjrigg@gmail.com>
2022-06-23 11:50:22 -0500

    Prove that the leak detector is working. Add some sample code to verify this

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Development Path:
 1. Test suite evaluation (Conversion to Google Test)
 2. Network Creation
    - [x] Proof of concept (Nodes and Elements)
-   - [ ] Evaluate memory safety of the network 
+   - [x] Evaluate memory safety of the network 
 3. Network Simplification (Resistors in DC)
 4. Loop Detection
 5. KVL Analysis
--- test/test-runner.cpp
+++ test/test-runner.cpp
@@ -7,12 +7,35 @@ int main(int argc, char** argv)
 }
 
 
-// Uncomment to sanity check the leak detector
-// #define CHECK_FOR_MEMORY_LEAKS
-#ifdef CHECK_FOR_MEMORY_LEAKS
+/**********************************************************************************************//**
+ * The following is an example of a circular dependency that causes a memory leak when implemented
+ * with std::shared_ptr's. When the two standalone pointers leave the test scope, they are
+ * destructed as expected. This destruction reduces the reference count on each pointer by 1. 
+ * Critically, each reference count was initially 2, due to the circular pointing. Because of this,
+ * the two underlying structs both remain in memory with their pointers each referencing each other
+ *************************************************************************************************/
+//#define GIVE_ME_MEMORY_LEAKS // Uncomment to sanity check the leak detector
+#ifdef GIVE_ME_MEMORY_LEAKS
+#include <memory>
+
+struct B;
+struct A
+{
+    std::shared_ptr<B> ptr;
+};
+
+struct B
+{
+    std::shared_ptr<A> ptr;
+};
+
 TEST(Checkout, SanityCheckLeakDetector)
 {
-    int* ptr = new int { 10 };
-    ptr = nullptr;
+    auto a = std::make_shared<A>();
+    auto b = std::make_shared<B>();
+
+    a->ptr = b;
+    b->ptr = a;
 }
+
 #endif
